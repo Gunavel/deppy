820ec89865cbab5e45e482f9cff9e3bc37ee7d8c
Gunavel <gunavel.bharathi@gmail.com>
2020-10-23 13:01:02 +0530

    cleanup and add spinner

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # deppy
 
-A module to cleanup dependencies
+A module to purge the unused dependencies from package.json.
+
+## Usage
+
+Run the below `npx` command from the project root where the package.json file is present.
+
+```node
+npx https://github.com/Gunavel/deppy
+```
+
+## Caution
+
+- Make sure to test the application manually once the dependencies are purged
+- Read <https://github.com/depcheck/depcheck#false-alert>
+
+## Known Issues
+
+- Running depcheck as spawn cmd in win32 systems seems to fail
--- depcheck.js
+++ depcheck.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-console */
 /*
   Analyze installed dependencies using `depcheck` npm module.
   1. Runs `depcheck` to find unused dependencies/devDependencies
-  2. Result array of unused dependencies are passed to `deps-processor.js` for purging safely
+  2. Array of unused dependencies are passed to `deps-processor.js` for purging safely
 
-  Usage:
-  - run `node depcheck.js` and look for any logs/errors for further info.
-  - (or)
-  - run using `npm run depcheck` which does the same
+  Usage: See readme for usage info.
 
   Known Issues:
   - running depcheck as spawn cmd in win32 systems seems to fail
 */
 const depcheck = require("depcheck");
+const ora = require("ora");
 
 if (process.platform === "win32") {
   console.log("<".repeat(50));
@@ -27,54 +24,49 @@ if (process.platform === "win32") {
 // dependency processor local module
 // const depsProcessor = require("./deps-processor");
 
+const spinner = ora("Collecting unused dependencies").start();
+
 try {
   const options = {
     skipMissing: true,
     ignoreDirs: ["dist", "build", "public", "_public"],
   };
 
-  console.log("-".repeat(50));
-  console.log("\nRunning depcheck...\n");
-
-  // Run depcheck npm module with optional flags
+  // Runs depcheck npm module with optional flags
   // see https://www.npmjs.com/package/depcheck for additional configs
   depcheck(process.cwd(), options, (unused) => {
+    spinner.succeed();
+
     const deps = [...unused.dependencies, ...unused.devDependencies];
     if (deps.length === 0) {
-      console.log("-".repeat(50));
-      console.log("👍 No unused dependencies in your project.");
-      console.log("-".repeat(50));
+      console.log("\n🔥 No unused dependencies found in your project. 🚀\n");
       process.exit();
     }
 
-    console.log("-".repeat(50));
     console.log(
       `Found ${
         unused.dependencies.length
       } unused dependencies. ${unused.dependencies.join(", ")}`
     );
+    console.log("\n");
     console.log(
       `Found ${
         unused.devDependencies.length
       } unused devDependencies. ${unused.devDependencies.join(",")}`
     );
-    console.log("-".repeat(50));
 
     // Process the collected dependencies
     // depsProcessor(deps);
   });
 } catch (e) {
-  console.log("<".repeat(50));
-  console.error("Failed to exec depcheck.\n");
+  spinner.fail("Failed to run depcheck.\n");
 
-  console.log("Node version: ", process.version);
   const currentNodeMajorVersion = Number(process.version.match(/^v(\d+\.)/)[1]);
-
   if (currentNodeMajorVersion < 10) {
-    console.error("Node version should be >=10 for depcheck to run.");
+    console.log("Node version: ", process.version);
+    console.log("Node version should be >=10 for depcheck to run.");
   }
 
-  console.log("Make sure to 'npm i -D depcheck' before running this file.\n");
-  console.log(">".repeat(50));
+  console.log(e);
   process.exit(1);
 }
