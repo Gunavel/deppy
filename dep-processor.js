9924a0279822d191fe41325ea3577281fc3b3cb2
Gunavel <gunavel.bharathi@gmail.com>
2020-10-27 10:54:31 +0530

    add eslint

--- dep-processor.js
+++ dep-processor.js
@@ -10,86 +10,86 @@
   Known Issues:
   - running depcheck as spawn cmd in win32 systems seems to fail
 */
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-const depcheck = require("depcheck");
-const ora = require("ora");
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const depcheck = require('depcheck')
+const ora = require('ora')
 
-if (process.platform === "win32") {
-  console.log("\nWindows sucks!! Please run this on linux based OS.\n");
-  process.exit(1);
+if (process.platform === 'win32') {
+  console.log('\nWindows sucks!! Please run this on linux based OS.\n')
+  process.exit(1)
 }
 
 /**
  * Commands
  */
-const uninstallDepCmd = (depsList) => `yarn remove ${depsList}`;
-const buildCmd = "yarn build";
+const uninstallDepCmd = (depsList) => `yarn remove ${depsList}`
+const buildCmd = 'yarn build'
 
 /** Processing indicator */
-const spinner = ora("Collecting unused dependencies").start();
+const spinner = ora('Collecting unused dependencies').start()
 
 try {
   const options = {
     skipMissing: true,
-    ignoreDirs: ["dist", "build", "public", "_public"],
-  };
+    ignoreDirs: ['dist', 'build', 'public', '_public']
+  }
 
   /**
    * Runs depcheck npm module with optional flags
    * See https://www.npmjs.com/package/depcheck for additional configs
    */
   depcheck(process.cwd(), options, async (unused) => {
-    spinner.succeed();
+    spinner.succeed()
 
-    const deps = unused.dependencies;
+    const deps = unused.dependencies
     if (deps.length === 0) {
-      console.log("\n🔥 No unused dependencies found in your project. 🚀\n");
-      process.exit();
+      console.log('\n🔥 No unused dependencies found in your project. 🚀\n')
+      process.exit()
     }
 
-    console.log(`\nUnused Dependencies: ${unused.dependencies.length}`);
-    console.dir(unused.dependencies);
+    console.log(`\nUnused Dependencies: ${unused.dependencies.length}`)
+    console.dir(unused.dependencies)
 
-    console.log("\n");
-    spinner.start("Purging dependencies");
-    console.log("\n");
+    console.log('\n')
+    spinner.start('Purging dependencies')
+    console.log('\n')
 
     try {
       /**
        * Uninstall the dependencies
        */
-      const depsList = deps.join(" ");
-      await exec(uninstallDepCmd(depsList));
-      spinner.succeed("Dependencies purged successfully!");
+      const depsList = deps.join(' ')
+      await exec(uninstallDepCmd(depsList))
+      spinner.succeed('Dependencies purged successfully!')
 
       /**
        * Run build command
        */
-      spinner.start("Checking if build succeeds");
-      await exec(buildCmd);
+      spinner.start('Checking if build succeeds')
+      await exec(buildCmd)
       spinner.succeed(
-        "Build succeeded! Please build/run the application manually to verify."
-      );
+        'Build succeeded! Please build/run the application manually to verify.'
+      )
     } catch (error) {
-      spinner.fail("Failed to purge dependencies.");
+      spinner.fail('Failed to purge dependencies.')
       console.log(
-        "Undo the package.json changes if any and run yarn install manually!"
-      );
-      console.log(error);
+        'Undo the package.json changes if any and run yarn install manually!'
+      )
+      console.log(error)
     }
 
-    process.exit();
-  });
+    process.exit()
+  })
 } catch (error) {
-  spinner.fail("Failed to run depcheck.\n");
+  spinner.fail('Failed to run depcheck.\n')
 
-  const currentNodeMajorVersion = Number(process.version.match(/^v(\d+\.)/)[1]);
+  const currentNodeMajorVersion = Number(process.version.match(/^v(\d+\.)/)[1])
   if (currentNodeMajorVersion < 10) {
-    console.log("Node version: ", process.version);
-    console.log("Node version should be >=10 for depcheck to run.");
+    console.log('Node version: ', process.version)
+    console.log('Node version should be >=10 for depcheck to run.')
   }
 
-  console.log(error);
-  process.exit(1);
+  console.log(error)
+  process.exit(1)
 }
